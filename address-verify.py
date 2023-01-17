c9a0361a22e92f3de90eee1acc217e062196e0f2
victd <55512729+victd@users.noreply.github.com>
2023-01-17 10:40:00 -0500

    Update address-verify.py

--- address-verify.py
+++ address-verify.py
@@ -1,7 +1,7 @@
 import smtplib
 
 # SMTP address verify
-# from HHOF rollcall, DIGITS, BI relaunch filter
+# from HHOF rollcall, DIGITS, BI relaunch SP exclusions
 
 server = smtplib.SMTP('mail')
 server.set_debuglevel(True)  # show communication with the server
